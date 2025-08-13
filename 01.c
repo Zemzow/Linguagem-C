47c2ebc114e678ff22f2dfa82b7750db3e1ec8a9
Enzo Martinêz Maximiano Cardoso <131972465+Zemzow@users.noreply.github.com>
2025-08-13 14:54:13 +0000

    Adição do 01.c para um Ola  Mundo através da variável A.

---
+++ 01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int A;
+
+    A = "Olá Mundo!"; //puxando variável 
+    
+    scanf("%d", &A);
+    printf("Olá Mundo!");
+    
+    
+    
+    return 0;
+}
