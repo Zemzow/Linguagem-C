e977c4f721ae0c67b84d2dca6d1995685e1b3c15
Enzo Martinêz Maximiano Cardoso <131972465+Zemzow@users.noreply.github.com>
2025-08-28 22:29:27 -0300

    Add files via upload

---
+++ Ex2.c
@@ -0,0 +1,15 @@
+//Soma de 2 números inteiros
+#include <stdio.h>
+
+int main()
+{
+    int n1 = 5;
+    int n2 =10;
+    
+    int soma = n1 + n2;
+    
+    scanf("%d", &soma);
+    printf("Soma de número 1 e número 2 é: ", soma);
+
+    return 0;
+}
